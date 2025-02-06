5c18673e16b2c0875c7676c68176503a448c4c4c
franmc01 <francmarinc@gmail.com>
2025-02-05 21:26:35 -0500

    feat: add scafold screens

---
+++ lib/presentation/screens/ui_controls.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class UiControls extends StatelessWidget {
+  static const name = 'ui_controls_screen';
+  
+  const UiControls({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
