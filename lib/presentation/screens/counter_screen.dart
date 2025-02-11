f694fb77d166031d4eccc34a276128d00a9eda5b
franmc01 <francmarinc@gmail.com>
2025-02-10 23:35:26 -0500

    fix: learn riverpod

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
 
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final textStyle = Theme.of(context).textTheme;
+    final int counterValue = ref.watch(counterProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Counter Screen'),
       ),
       body: Center(
-        child: Text('Value: 10', style: textStyle.titleLarge),
+        child: Text('Value: $counterValue', style: textStyle.titleLarge),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        // counterValue++;
+      }, child: Icon(Icons.add)),
     );
   }
 }
